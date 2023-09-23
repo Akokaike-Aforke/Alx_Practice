66b5ded0bd9d88d6c13a7a940f4fa459c2f909d2
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-23 12:46:05 +0100

    added test file

---
+++ alx-low_level_programming/0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/*
+ * puts2 - prints string
+ * @str - string reverse
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+}
+
+int main(void)
+{
+    char *str;
+
+    str = "0123456789";
+    puts2(str);
+    return (0);
+}
